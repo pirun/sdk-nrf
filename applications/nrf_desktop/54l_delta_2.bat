eb14e12b4fb43ee3c900386eac7a615e1554c812
Pirun Lee <pirun.lee@nordicsemi.no>
2024-12-06 08:56:02 +0800

    application: dfu: enable delta dfu
    Add prj_delta.conf for delta DFU BLE only.
    Add prj_delta_gazell_ble.conf for delta DFU BLE and Gazell coex.
    Add config files for sysbuild and partition manager.
    Add batch file to show how to use delta dfu.
    
    Signed-off-by: Pirun Lee <pirun.lee@nordicsemi.no>

---
+++ applications/nrf_desktop/54l_delta_2.bat
@@ -0,0 +1,21 @@
+@REM This script is used to build the update application and create delta patch binary. The delta patch is generated and copied to the delta_dfu/binaries/patches folder for update.
+
+@REM Build the application
+west build -p -b nrf54l15dk/nrf54l15/cpuapp -d build_54l -- -DFILE_SUFFIX=delta_gazell_ble -DCONFIG_MCUBOOT_IMGTOOL_SIGN_VERSION=\"2.0.0+0\"
+@REM Backup original hex file
+copy build_54l\merged.hex delta_200.hex
+@REM Generate signed binary image from hex for later delta patch generation
+arm-zephyr-eabi-objcopy --input-target=ihex --output-target=binary --gap-fill=0xff build_54l\nrf_desktop\zephyr\zephyr.signed.hex %ZEPHYR_BASE%\..\delta_dfu\binaries\signed_images\target_200.bin
+
+@REM Check if patches directory exists, if not create it
+setlocal
+:: Define the directory want to create
+set "dir=%ZEPHYR_BASE%\..\delta_dfu\binaries\patches"
+:: Check if the directory exists
+if not exist "%dir%" (
+    :: Create the directory and its parent directories
+    mkdir "%dir%"
+)
+endlocal
+@REM Generate delta patch binary with patch_with_arg.py script
+python  %ZEPHYR_BASE%\..\delta_dfu\scripts\patch_with_arg.py -d build_54l
